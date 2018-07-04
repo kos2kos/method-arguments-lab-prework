7bb6a440675d47cb8c829098bbddd799c9c147bd
Nkosi Enciso Givhan <koasterman@gmail.com>
2018-07-04 16:31:38 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,6 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def introdution_with_language(name, lang)
+def introduction_with_language(name, lang)
   puts "Hi, my name is #{name} and I am learning to program in #{lang}."
 end
